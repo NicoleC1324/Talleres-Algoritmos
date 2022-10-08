31d4a57db9c584e181801b24940c2708711cdfd6
NicoleC1324 <114431012+NicoleC1324@users.noreply.github.com>
2022-10-08 00:45:03 -0500

    x

---
+++ Taller Estructuras de Control Secuenciales/Ejercicio14.py
@@ -0,0 +1,7 @@
+ck=float(input("Costo por kilovatio: "))
+la=float(input("Lectura recibo anterior: "))
+lac=float(input("Lectura recibo actual: "))
+pla=la*ck
+plac=lac*ck
+print("Monto recibo lectura anterior: ",pla)
+print("Monto recibo lectura actual: ",plac)
