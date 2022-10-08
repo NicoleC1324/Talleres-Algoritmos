b1e33058039f0fe593819d5aa49b1a9f23b2f8d4
NicoleC1324 <114431012+NicoleC1324@users.noreply.github.com>
2022-10-07 23:43:35 -0500

    x

---
+++ Taller Estructuras de Control Secuenciales/Ejercicio16.py
@@ -0,0 +1,5 @@
+g=float(input("Número de galones vendidos: "))
+l=g*3.785
+pl=l*50000
+print("Litros recargados: ",l)
+print("Total costo gasolina: ",pl)
---
+++ Taller Estructuras de Control Secuenciales/Ejercicio17.py
@@ -0,0 +1,7 @@
+p=float(input("Presupuesto del hospital: "))
+g=p*0.40
+t=p*0.3
+pe=p*0.3
+print("Presupuesto ginecología: ",g)
+print("Presupuesto traumatología: ",t)
+print("Presupuesto pediatría: ",pe)
